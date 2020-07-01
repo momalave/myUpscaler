c4c4d4e92e319104e5d40742a0ddc7079aa44e0e
Mario Malave <momalave@gmail.com>
2020-07-01 01:24:49 -0700

    Update myUtils.h

--- include/myUtils.h
+++ include/myUtils.h
@@ -1,7 +1,7 @@
 // Created by Mario Malave
 
-#ifndef MYSCALER_MYUTILS_H
-#define MYSCALER_MYUTILS_H
+#ifndef MYUPSCALER_MYUTILS_H
+#define MYUPSCALER_MYUTILS_H
 
 using namespace cv;
 using namespace std;
@@ -15,4 +15,4 @@ void drawStatus(int curFrame, int numFrames, float dur);
 // Add audio to upsampled video
 void processAudio(string inputDir, string outputDir);
 
-#endif //MYSCALER_MYUTILS_H
+#endif //MYUPSCALER_MYUTILS_H
